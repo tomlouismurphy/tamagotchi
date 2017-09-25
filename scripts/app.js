344d8393002f4666197366119cb3ad0a0de3186e
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-09-25 16:46:51 -0500

    added metric display

--- scripts/app.js
+++ scripts/app.js
@@ -11,5 +11,15 @@ class Tamagotchi {
 		this.weight = Math.ceil(Math.random() * 5);
 		this.iq = Math.ceil((Math.random() * 40) + 80);
 		$('.container').prepend('<img id="sprite_1" src="images/defaultsprite.png" />')
+		for (let i = 0; i < 4; i++){
+			const $p = $('<p></p>', {id: 'stat-' + i})
+			$p.addClass('stats');
+			$('.container').append($p);
+		}
+		$('#stat-0').text('Age: ' + this.age);
+		$('#stat-1').text('Hunger: ' + this.hunger);
+		$('#stat-2').text('Sleepiness: ' + this.sleepiness);
+		$('#stat-3').text('Boredom: ' + this.boredom);
 	}
 }
+
