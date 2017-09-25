a695c2d4e0df71ee170b7f3e51afb4e28c9d7cb0
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-09-25 16:58:25 -0500

    added buttons

--- scripts/app.js
+++ scripts/app.js
@@ -11,15 +11,25 @@ class Tamagotchi {
 		this.weight = Math.ceil(Math.random() * 5);
 		this.iq = Math.ceil((Math.random() * 40) + 80);
 		$('.container').prepend('<img id="sprite_1" src="images/defaultsprite.png" />')
-		for (let i = 0; i < 4; i++){
-			const $p = $('<p></p>', {id: 'stat-' + i})
-			$p.addClass('stats');
-			$('.container').append($p);
-		}
-		$('#stat-0').text('Age: ' + this.age);
-		$('#stat-1').text('Hunger: ' + this.hunger);
-		$('#stat-2').text('Sleepiness: ' + this.sleepiness);
-		$('#stat-3').text('Boredom: ' + this.boredom);
 	}
-}
+};
 
+const gameSetup = () => {
+	for (let i = 0; i < 4; i++){
+		const $p = $('<p></p>', {id: 'stat-' + i})
+		$p.addClass('stats');
+		$('.container').append($p);
+	}
+	$('#stat-0').text('Age: ' + 0);
+	$('#stat-1').text('Hunger: ' + 0);
+	$('#stat-2').text('Sleepiness: ' + 0);
+	$('#stat-3').text('Boredom: ' + 0);
+	for (let i = 0; i < 3; i++){
+		const $button = $('<button></button>', {id: 'action-' + i})
+		$button.addClass('interact');
+		$('.container').append($button);
+	}
+	$('#action-0').text('Feed the beast');
+	$('#action-1').text('Nighty night!');
+	$('#action-2').text('Playtime');
+};
