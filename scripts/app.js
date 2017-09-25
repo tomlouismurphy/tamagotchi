1443191986663861a55490c78dfe17aef8ee91e2
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-09-25 15:52:32 -0500

    defined tamagotchi class

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+class Tamagotchi {
+	constructor(name, favoriteFoods, ){
+		this.name = name;
+		this.hunger = 0;
+		this.sleepiness = 0;
+		this.boredom = 0;
+		this.age = 0;
+		this.favoriteFoods = favoriteFoods;
+		this.healthy = true;
+		this.lifeStage = 'baby';
+		this.weight = Math.ceil(Math.random() * 5);
+		this.iq = Math.ceil((Math.random() * 40) + 80);
+	}
+}
